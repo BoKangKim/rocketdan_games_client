ff43bc67d3b6fa15e0a9aa5fd85be3e4a67da6cf
BoKangKim <bkbk0329@naver.com>
2025-03-27 04:26:26 +0900

    WIP : Fix Collision Exception
    WIP : Fix Collision Exception

--- Assets/2.Scripts/Monster/Monster.cs
+++ Assets/2.Scripts/Monster/Monster.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Game.Data;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Game.Entity
 {
@@ -16,11 +17,11 @@ namespace Game.Entity
         protected MonsterDataScriptable data = null;
         protected Transform target = null;
 
-        protected bool isJump = false;
-        public bool IsJump => isJump;
+        protected bool isOnFloor = false;
+        public bool IsOnFloor => isOnFloor;
 
-        [SerializeField] protected bool isOnMonster = false;
-        public bool IsOnMonster => isOnMonster;
+        [SerializeField] protected Monster collisionMonster = null;
+        public bool CanCollision => collisionMonster == null;
 
         private void OnDisable()
         {
@@ -51,10 +52,99 @@ namespace Game.Entity
             this.data = data;
             this.target = target;
             UpdateModel(data);
+            rigid.velocity = Vector2.left * 3f;
+        }
+
+        public void ResetCollision()
+        {
+            collisionMonster = null;
         }
 
         protected abstract void Move();
         protected abstract bool RangeCheck();
         protected abstract void Attack();
+
+        protected virtual void CollisionEnter(Collision2D collision)
+        {
+            if (collision.transform.tag.Equals("Floor"))
+            {
+                rigid.mass = 1;
+                isOnFloor = true;
+                return;
+            }
+
+            if (!CanCollision || !isOnFloor)
+            {
+                return;
+            }
+
+            collisionMonster = collision.transform.GetComponent<Monster>();
+
+            if (collisionMonster != null)
+            {
+                if (!collisionMonster.IsOnFloor || !isOnFloor || !collisionMonster.CanCollision)
+                {
+                    collisionMonster = null;
+                    return;
+                }
+
+                collisionMonster.ForceSet(this);
+            }
+        }
+
+        public void ForceSet(Monster monster)
+        {
+            this.collisionMonster = monster;
+        }
+
+        protected virtual void CollisionExit(Collision2D collision)
+        {
+            if (collision.transform.tag.Equals("Floor"))
+            {
+                isOnFloor = false;
+
+            }
+        }
+
+        protected virtual void Climb()
+        {
+            if (collisionMonster == null)
+            {
+                return;
+            }
+
+            if (!collisionMonster.IsOnFloor)
+            {
+                return;
+            }
+
+            float yDistance = Mathf.Abs(collisionMonster.transform.position.y - transform.position.y);
+            if (yDistance > 1f)
+            {
+                collisionMonster.ResetCollision();
+                collisionMonster = null;
+                return;
+            }
+
+            if (collisionMonster.transform.position.x <= transform.position.x)
+            {
+                Vector2 climbDir = Vector2.left + Vector2.up;
+                climbDir = climbDir.normalized;
+                rigid.velocity = climbDir * 3f;
+
+
+                return;
+            }
+
+            if (!isOnFloor)
+            {
+                rigid.mass = 9f;
+                // rigid.AddForce(Vector2.left + Vector2.down, ForceMode2D.Impulse);
+                // collisionMonster.Rigid.AddForce(Vector2.right, ForceMode2D.Impulse);
+                collisionMonster.ResetCollision();
+                collisionMonster.Rigid.velocity = Vector2.left;
+                collisionMonster = null;
+            }
+        }
     }
 }
--- Assets/2.Scripts/Monster/Zombie/ZombieMelee.cs
+++ Assets/2.Scripts/Monster/Zombie/ZombieMelee.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Game.Data;
-using Game.Util;
 using UnityEngine;
 
 namespace Game.Entity
@@ -17,50 +16,9 @@ namespace Game.Entity
         [SerializeField] private SpriteRenderer leftLeg;
         [SerializeField] private SpriteRenderer rightLeg;
 
-        private Vector2 direction = Vector2.zero;
-
-        private Vector2 startPos = Vector2.zero;
-        private Vector2 targetPos = Vector2.zero;
-        private float time = 0f;
-        private float speed = 0f;
-
-        private Monster collisionMonster = null;
-
         public override void Init(MonsterDataScriptable data, Transform target)
         {
             base.Init(data, target);
-            direction = Vector2.left;
-            speed = data.Speed;
-        }
-
-        protected override void Update()
-        {
-            if (isJump)
-            {
-                Jump();
-                return;
-            }
-
-            if (collisionMonster != null && !collisionMonster.IsJump)
-            {
-                if (!isOnMonster && Mathf.Abs(transform.position.y - collisionMonster.transform.position.y) > 0.1f && transform.position.y < collisionMonster.transform.position.y)
-                {
-                    isOnMonster = true;
-                    direction = Vector2.right;
-                    speed /= 2f;
-                }
-
-                if (isOnMonster && Mathf.Abs(transform.position.x - collisionMonster.transform.position.x) > 0.8f)
-                {
-                    collisionMonster = null;
-                    isOnMonster = false;
-                    direction = Vector2.left;
-                    speed = data.Speed;
-                }
-
-            }
-
-            base.Update();
         }
 
         protected override void Attack()
@@ -71,8 +29,7 @@ namespace Game.Entity
 
         protected override void Move()
         {
-            Vector2 velo = new Vector2(direction.x * speed, rigid.velocity.y);
-            rigid.velocity = velo;
+            Climb();
         }
 
         protected override bool RangeCheck()
@@ -98,94 +55,13 @@ namespace Game.Entity
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
-            if (isJump || collisionMonster != null)
-            {
-                return;
-            }
-
-            if (collision.transform.TryGetComponent(out collisionMonster))
-            {
-                if (!isOnMonster)
-                {
-                    rigid.velocity = Vector2.zero;
-                }
-
-                if (collisionMonster.IsJump || collisionMonster.IsOnMonster)
-                {
-                    return;
-                }
-
-                if (transform.position.x > collisionMonster.transform.position.x && Mathf.Abs(transform.position.y - collisionMonster.transform.position.y) < 0.1f)
-                {
-                    startPos = transform.position;
-                    targetPos = collisionMonster.transform.position;
-                    targetPos.y += 1f;
-                    rigid.velocity = Vector2.zero;
-                    isJump = true;
-                }
-
-
-
-
-                // if (transform.position.y < monster.transform.position.x)
-                // {
-                //     direction = Vector2.right;
-                // }
-
-                // if (transform.position.y > monster.transform.position.y)
-                // {
-                //     isJump = false;
-                // }
-            }
+            CollisionEnter(collision);
         }
 
-        // private void OnCollisionExit2D(Collision2D collision)
-        // {
-        //     if (collision.transform.TryGetComponent(out Monster monster))
-        //     {
-        //         if (collisionMonster == null || monster.GetComponentIndex() != collisionMonster.GetComponentIndex())
-        //         {
-        //             return;
-        //         }
-
-        //         direction = Vector2.left;
-        //         collisionMonster = null;
-        //     }
-        // }
-
-        private void Jump()
+        private void OnCollisionExit2D(Collision2D collision)
         {
-            if (targetPos == Vector2.zero)
-            {
-                isJump = false;
-                time = 0f;
-                return;
-            }
-
-            time += Time.deltaTime * 0.8f;
-
-            if (time > 1f)
-            {
-                time = 0f;
-                isJump = false;
-                return;
-            }
-            rigid.MovePosition(CalculateBezier(time, 1f));
+            CollisionExit(collision);
         }
 
-        private Vector3 CalculateBezier(float time, float height)
-        {
-            Vector3 p0 = startPos;
-            Vector3 p2 = targetPos;
-            Vector3 p1 = (p0 + p2) / 2f;
-            p1.y += height;
-
-            Vector3 lerp1 = Vector3.Lerp(p0, p1, time);
-            Vector3 lerp2 = Vector3.Lerp(p1, p2, time);
-
-            Vector3 result = Vector3.Lerp(lerp1, lerp2, time);
-
-            return result;
-        }
     }
 }
